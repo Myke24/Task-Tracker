532eba1282da373d38b62013d04558c184ef4e87
Myke24 <mykiel.d.horn24@gmail.com>
2022-10-04 19:34:41 -0500

    CRUD completed
    Entries can be created, read, updated and deleted from the database via client side

--- Task_Tracker/Controllers/EntryController.cs
+++ Task_Tracker/Controllers/EntryController.cs
@@ -28,6 +28,11 @@ namespace Task_Tracker.Controllers
             return View(entries);
         }
 
+        public IActionResult Add()
+        {
+            return View();
+        }
+
         [HttpPost]
         public async Task<IActionResult> Add(AddEntryViewModel addEntryRequest)
         {
@@ -45,6 +50,64 @@ namespace Task_Tracker.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var entry = await _db.Entries.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entry != null)
+            {
+                var viewModel = new EditViewModel()
+                {
+                    Id = entry.Id,
+                    Title = entry.Title,
+                    Description = entry.Description,
+                    StartDate = entry.StartDate,
+                    EndDate = entry.EndDate,
+                };
+
+                return View(viewModel);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditViewModel model)
+        {
+            var entry = await _db.Entries.FindAsync(model.Id);
+            if (entry != null)
+            {
+                entry.Title = model.Title;
+                entry.Description = model.Description;
+                entry.StartDate = model.StartDate;
+                entry.EndDate = model.EndDate;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            //Change this redirect to an error view because at this point an entry wasn't found and the _db returned null
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(EditViewModel model)
+        {
+            var entry = await _db.Entries.FindAsync(model.Id);
+            if (entry != null)
+            {
+                _db.Entries.Remove(entry);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            //Change this redirect to an error view because at this point an entry wasn't found and the _db returned null
+            return RedirectToAction("Index");
+        }
     }
 }
 
---
+++ Task_Tracker/Models/EditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Task_Tracker.Models
+{
+    public class EditViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
+
